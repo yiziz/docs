482a6eb867217c55a5784e027e039350770f915e
Chris Shaw <me@cshaw.xyz>
2021-04-26 18:18:22 +1000

    wip

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,16 +1,55 @@
 module.exports = {
-  base: '/aem-vite/',
+  base: '/',
   title: 'AEM Vite',
   description: 'AEM Vite is a plugin for AEM that enables transparent support for the Vite DevServer.',
 
   head: [
-    [
-      'link',
-      {
-        rel: 'shortcut icon',
-        href: '/aem-vite/favicon.ico',
-      },
-    ],
+    ['link', {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    }],
+    ['link', {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    }],
+    ['link', {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    }],
+    ['link', {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    }],
+    ['link', {
+      rel: 'mask-icon',
+      href: '/safari-pinned-tab.svg',
+      color: '#c34c5c',
+    }],
+    ['link', {
+      rel: 'shortcut icon',
+      href: '/favicon.ico',
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-title',
+      content: 'AEM Vite',
+    }],
+    ['meta', {
+      name: 'application-name',
+      content: 'AEM Vite',
+    }],
+    ['meta', {
+      name: 'msapplication-TileColor',
+      content: '#c34c5c',
+    }],
+    ['meta', {
+      name: 'theme-color',
+      content: '#ffffff',
+    }],
   ],
 
   themeConfig: {
@@ -26,14 +65,17 @@ module.exports = {
     lastUpdated: false,
 
     nav: [
-      // { text: 'Home', link: '/' },
+      {
+        text: 'Front End',
+        link: '/guide/front-end/',
+      },
     ],
 
     sidebar: {
       '/guide/': getGuideSidebar(),
-      '/': getGuideSidebar()
-    }
-  }
+      '/': getGuideSidebar(),
+    },
+  },
 }
 
 function getGuideSidebar() {
@@ -53,20 +95,20 @@ function getGuideSidebar() {
           text: 'Configuration',
           link: '/guide/configuration/',
         },
-      ]
+      ],
     },
     {
       text: 'Front End',
       children: [
         { text: 'Preparing your project', link: '/guide/front-end/' },
-      ]
+      ],
     },
     {
       text: 'Extending AEM Vite',
       children: [
         { text: 'Why extend AEM Vite?', link: '/guide/extending/why/' },
         { text: 'Super-powered ClientLibs', link: '/guide/extending/clientlibs/' },
-      ]
+      ],
     },
   ]
 }
