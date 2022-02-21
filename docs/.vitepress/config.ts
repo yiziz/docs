29b7021590ce928ee03dd55a57bed4b18753e7d1
Chris Shaw <me@cshaw.xyz>
2022-02-22 10:28:04 +1100

    docs: minor config tweaks

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,265 @@
+import { defineConfigWithTheme, type UserConfig } from 'vitepress';
+
+const title = 'AEM Vite';
+const description =
+  'AEM Vite brings the amazing Vite ecosystem to AEM in a transparent and friendly way.';
+
+function getGuideSidebar() {
+  return [
+    {
+      text: 'Guide',
+      children: [
+        {
+          text: 'Overview',
+          link: '/guide/',
+        },
+      ],
+    },
+    {
+      text: 'Backend (AEM)',
+      children: [
+        {
+          text: 'Installation',
+          link: '/guide/backend/installation/',
+        },
+        {
+          text: 'Configuration',
+          link: '/guide/backend/configuration/',
+        },
+        {
+          text: 'Permissions',
+          link: '/guide/backend/permissions/',
+        },
+      ],
+    },
+    {
+      text: 'Front End',
+      children: [
+        { text: 'Preparing your project', link: '/guide/front-end/' },
+        { text: 'Structure', link: '/guide/front-end/structure/' },
+        { text: 'Vite Configuration', link: '/guide/front-end/vite/' },
+        { text: 'Vite Plugin', link: '/guide/front-end/vite-plugin/' },
+        { text: 'Module Imports', link: '/guide/front-end/module-imports/' },
+        { text: 'Dynamic Imports', link: '/guide/front-end/dynamic-imports/' },
+        { text: 'Static Assets', link: '/guide/front-end/static-assets/' },
+      ],
+    },
+    // {
+    //   text: 'Extending AEM Vite',
+    //   children: [
+    //     { text: 'Why extend AEM Vite?', link: '/guide/extending/why/' },
+    //     { text: 'Super-powered ClientLibs', link: '/guide/extending/clientlibs/' },
+    //   ],
+    // },
+    {
+      text: 'Legacy Bundles',
+      children: [
+        { text: 'What are they?', link: '/guide/legacy-bundles/' },
+        {
+          text: 'ClientLibs support',
+          link: '/guide/legacy-bundles/clientlibs/',
+        },
+      ],
+    },
+    {
+      text: 'FAQs',
+      link: '/guide/faqs/',
+    },
+    {
+      text: 'Migrate to v2.x',
+      link: '/guide/migrate-to-v2/',
+    },
+  ];
+}
+
+export default defineConfigWithTheme({
+  base: '/',
+  title,
+  description,
+
+  head: [
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/static/apple-touch-icon.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/static/favicon-32x32.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/static/favicon-16x16.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'manifest',
+        href: '/static/site.webmanifest',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/static/safari-pinned-tab.svg',
+        color: '#c34c5c',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'shortcut icon',
+        href: '/static/favicon.ico',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'apple-mobile-web-app-title',
+        content: 'AEM Vite',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'application-name',
+        content: 'AEM Vite',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'msapplication-TileColor',
+        content: '#c34c5c',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'theme-color',
+        content: '#ffffff',
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'og:title',
+        content: title,
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'og:description',
+        content: description,
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'og:image',
+        content: '/static/social/og-primary.jpg',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'twitter:title',
+        content: title,
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'twitter:description',
+        content: description,
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'twitter:site',
+        content: '@cshawaus',
+      },
+    ],
+  ],
+
+  themeConfig: {
+    docsDir: 'docs',
+    docsBranch: 'main',
+    editLinks: false,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: false,
+    logo: 'static/logo.png',
+    repo: 'aem-vite',
+
+    algolia: {
+      apiKey: '0479a3a9031b5947143a70dc4969da0d',
+      indexName: 'aemvite',
+    },
+
+    nav: [
+      {
+        text: 'Installation',
+        link: '/guide/backend/installation/',
+      },
+      {
+        text: 'Front End',
+        link: '/guide/front-end/',
+      },
+      {
+        text: 'Legacy Bundles',
+        link: '/guide/legacy-bundles/',
+      },
+      {
+        text: 'FAQs',
+        link: '/guide/faqs/',
+      },
+      {
+        text: 'Changelogs',
+        items: [
+          {
+            text: 'Vite for AEM',
+            link: 'https://github.com/aem-vite/aem-vite/blob/main/CHANGELOG.md',
+          },
+          {
+            text: 'Vite AEM Plugin',
+            link: 'https://github.com/aem-vite/vite-aem-plugin/blob/main/CHANGELOG.md',
+          },
+          {
+            text: 'Vite Import Rewriter',
+            link: 'https://github.com/aem-vite/import-rewriter/blob/main/CHANGELOG.md',
+          },
+        ],
+      },
+    ],
+
+    sidebar: {
+      '/guide/': getGuideSidebar(),
+      '/': getGuideSidebar(),
+    },
+
+    footer: {
+      copyright: `Copyright © 2021-${new Date().getFullYear()} Chris Shaw`,
+
+      license: {
+        text: 'Apache-2.0 License',
+        link: 'https://www.apache.org/licenses/LICENSE-2.0',
+      },
+    },
+  },
+});
